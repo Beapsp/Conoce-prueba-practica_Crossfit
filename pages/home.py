60e649c5ad69a39bc2f8357499ca43fc2e0c7987
Beapsp <beasps@gmail.com>
2021-12-13 20:25:41 +0100

    16

--- pages/home.py
+++ pages/home.py
@@ -2,7 +2,7 @@ import streamlit as st
 from PIL import Image
 #import streamlit.components.v1 as components
 #import codecs
-
+#gif = 'https://i.pinimg.com/originals/74/65/1d/74651dc9fc122bf6847c597d97da7ed0.gif'
 
 def app():
 
@@ -10,9 +10,12 @@ def app():
     # Conoce, prueba, practica CROSSFIT
     
     """)
-    portada = Image.open("imagenes/image1.jpg")
+    portada = Image.open("imagenes/image5.jpg")
+    #portada = Image.open(gif)
     st.image(portada, use_column_width=True)
     #f=codecs.open("data/pedrito.html", 'r')
     #pedro = f.read()
 
     #components.html(pedro,height=550,scrolling=True)
+
+
--- pages/movimientos.py
+++ pages/movimientos.py
@@ -4,11 +4,20 @@ import sys
 sys.path.append('../')
 
 
-
+    
 
 def app():
-    st.write("Hola")
-    movimiento = st.selectbox("Elige un movimiento",["hola", "otro", "pressjwdgoiwh", "junkdasgkwhjdgoi"])
-    st.write(movimiento)
-    df_mov_video = pd.read_csv("../data/movimientos_video.csv",encoding = "ISO-8859-1")
+    data = pd.read_csv("data/movimientos_video.csv")
+
+    #st.write("Hola")
+    #movimiento = st.selectbox("Elige un movimiento",["hola", "otro", "pressjwdgoiwh", "junkdasgkwhjdgoi"])
+    #st.write(movimiento)
+    #df_mov_video = pd.read_csv("../data/movimientos_video.csv")
+    #data = pd.read_csv("../data/movimientos_video.csv")
+    st.dataframe(data)
+   
+
+    size = data.name.unique()
+    input_size = st.selectbox("¿Qué movimiento quieres conocer?", size)
+
    
